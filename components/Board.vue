3b81233142f1b85fc53b657ef337361722b1ff7b
AnaLinsDev <analinsdev@gmail.com>
2023-04-17 10:34:40 -0300

    add design

---
+++ components/Board.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-col cols="5">
+    <v-card height="calc(100vh - 60px)" elevation="0"> teste </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/Monitor.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-col cols="2">
+    <v-card
+      height="calc(100vh - 60px)"
+      color="transparent"
+      elevation="0"
+      class="d-flex align-center"
+    >
+      <v-card height="20vh" min-width="100%" elevation="0"> monitor </v-card>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
